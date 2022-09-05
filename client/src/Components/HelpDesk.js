8d1c0b3d1ba78f04401eb7b361265e9058b1c55a
palak4207 <63452981+palak4207@users.noreply.github.com>
2022-09-05 16:17:19 +0530

    Dynamic routing for helpDesk

--- client/src/Components/HelpDesk.js
+++ client/src/Components/HelpDesk.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import './../Assets/css/HelpDesk.css';
+import IssuerSidebar from './IssuerSidebar';
 import ValidatorSidebar from './ValidatorSidebar';
 
 export default function HelpDesk() {
+  const [userType, setUserType] = useState();
+
+  useEffect(() => {
+    setUserType(JSON.parse(localStorage.getItem("UserCredentials")).type);
+  }, []);
   return (
     <>
       <section className='help-banner text-light'>
-        <ValidatorSidebar/>
+      {userType === "issuer" ? <IssuerSidebar/> : <ValidatorSidebar />}
         <div className='help-banner text-light Help-section'>
             <div className='container help-container'>
             <div className='help-Header text-center text-center p-5'>
