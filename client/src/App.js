8c1efa682abcdecfb945d28d796c90796aafa39b
Khilesh412 <khilesh.sarode@nanostuffs.com>
2022-11-03 12:43:00 +0530

    Qr-Scanner-UI

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,8 @@ import Protected from "./Components/Protected";
 import PageNotFound from "./Components/PageNotFound";
 import ForgetPassword from "./Components/ForgetPassword";
 import Transaction from "./Components/Transaction";
-import QrScanner from "./Components/QrScanner";
+import QrCodeScanner from "./Components/QrCodeScanner";
+import QrScan from "./Components/QrScan";
 
 function App() {
   return (
@@ -54,11 +55,9 @@ function App() {
           />
           <Route path="/pagenotfound" exact element={<PageNotFound />} />
           <Route path="/ForgetPassword" exact element={<ForgetPassword />} />
-          <Route
-            path="/Transaction"
-            exact
-            element={<Protected component={Transaction} type="issuer" />}
-          />
+          <Route path="/qrscanner" exact element={<QrCodeScanner/>} />
+          <Route path="/qrscan" exact element={<QrScan/>} />
+          <Route path="/Transaction" exact element={<Protected component={Transaction} type="issuer"/>} />
         </Routes>
       </Router>
     </>
